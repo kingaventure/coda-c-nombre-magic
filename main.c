369b8a755c49c6f40f6accaf10a6822bc9a78da3
root <root@Lenovogroult>
2024-09-26 15:45:23 +0200

    Correction pour 3 conditions

--- main.c
+++ main.c
@@ -7,9 +7,10 @@ int main()
 	srand(time(NULL));
         int random_number = rand() % 100 + 1;
 	int nombre_devine;
+	int validation=0;
 	printf("Bonjour veuillez choisir un chiffre entre 1 et 100\n");
 	scanf("%d", &nombre_devine);
-	while(random_number != nombre_devine){
+	while(validation==0){
 	if(nombre_devine < random_number){
 	printf("Le nombre mystère est plus grand !\n");
 	scanf("%d", &nombre_devine);
@@ -18,9 +19,10 @@ int main()
 	printf("Le nombre mystère est plus petit !\n");
         scanf("%d", &nombre_devine);
 	}
-	}
-	if(random_number == nombre_devine){
+	else{
+	validation++;
 	printf("Bravo vous avez trouvez le numéro mystère\n");
+	}
 	}
 	exit(0);
 }
